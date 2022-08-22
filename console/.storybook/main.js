b27800d9dbb9a23e786f4af03f4aa677193b85b0
waynelwz <wytheme.go@gmail.com>
2022-08-22 18:59:55 +0800

    wip: make storbook work

--- console/.storybook/main.js
+++ console/.storybook/main.js
@@ -4,8 +4,8 @@ const path = require('path')
 module.exports = {
     stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
     addons: [
-        // '@storybook/addon-links',
-        // '@storybook/addon-essentials',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
         // '@storybook/addon-interactions',
         // '@storybook/preset-create-react-app',
     ],
@@ -20,9 +20,8 @@ module.exports = {
         const { config: userConfig } = await loadConfigFromFile(path.resolve(__dirname, '../vite.config.ts'))
 
         return mergeConfig(config, {
-            ...userConfig,
-            // manually specify plugins to avoid conflict
-            // plugins: [],
+            // server: userConfig.server,
+            resolve: userConfig.resolve,
         })
     },
 }
